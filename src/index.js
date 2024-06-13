101718da2d78539d165aae3bf820d1fbfcf5bb3d
minhddhe172880 <minhddhe172880@fpt.edu.vn>
2024-06-13 22:43:45 +0700

    Course Single

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './Components/Homepage/Homepage';
 import CoursePage from './Components/CoursePage/CoursePage';
+import CourseSingle from './Components/CourseSingle/CourseSingle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Homepage/> */}
     {/* <CoursePage/> */}
+    <CourseSingle/>
   </React.StrictMode>
 )

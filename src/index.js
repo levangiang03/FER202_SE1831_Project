4357222b61a72d77eac0ea73fcf71c8ba1d28b9c
levangiang03 <128107714+levangiang03@users.noreply.github.com>
2024-06-16 23:40:47 +0700

    Src_16/6/2024

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './Components/Homepage/Homepage';
 import CoursePage from './Components/CoursePage/CoursePage';
+<<<<<<< Updated upstream
+=======
+import CourseSingle from './Components/CourseSingle/CourseSingle';
+import courseslist, { CoursesBody, CoursesBody2 } from './Components/CourseList/courseslist';
+import createcourse from './Components/CreateCourse/createcourse';
+import Login_Register from './Components/Login_Register/Login_Register';
+import Profile from './Components/Profile/Profile';
+import Purchases from './Components/Purchases/Purchases';
+import CreateCourse from './Components/CreateCourse/createcourse';
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +20,14 @@ root.render(
     {/* <Homepage/> */}
     {/* <CoursePage/> */}
     {/* <CourseSingle/> */}
+<<<<<<< Updated upstream
+=======
+    {/* <Login_Register/> */}
+    {/* <CoursesBody1/> */}
+    {/* <CoursesBody2/> */}
+    {/* <CreateCourse/> */}
+    {/* <Profile/> */}
+    {/* <Purchases/> */}
+>>>>>>> Stashed changes
   </React.StrictMode>
 )

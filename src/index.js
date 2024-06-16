f8fffc372d7c23c36b9bd003212d78f6f0b788a3
levangiang03 <128107714+levangiang03@users.noreply.github.com>
2024-06-16 23:47:22 +0700

    Src_16/6

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './Components/Homepage/Homepage';
 import CoursePage from './Components/CoursePage/CoursePage';
 import CourseSingle from './Components/CourseSingle/CourseSingle';
+import courseslist, { CoursesBody, CoursesBody2 } from './Components/CourseList/courseslist';
+import createcourse from './Components/CreateCourse/createcourse';
+import Login_Register from './Components/Login_Register/Login_Register';
+import Profile from './Components/Profile/Profile';
+import Purchases from './Components/Purchases/Purchases';
+import CreateCourse from './Components/CreateCourse/createcourse';
+import Systems from './Components/Settings/Systems';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Homepage/> */}
     {/* <CoursePage/> */}
-    <CourseSingle/>
+    {/* <CourseSingle/> */}
+    {/* <Login_Register/> */}
+    {/* <CoursesBody1/> */}
+    {/* <CoursesBody2/> */}
+    {/* <CreateCourse/> */}
+    {/* <Profile/> */}
+    {/* <Purchases/> */}
+    {/* <Systems/> */}
   </React.StrictMode>
 )
